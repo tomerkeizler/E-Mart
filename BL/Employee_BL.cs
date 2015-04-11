e51b9aaa3dc3e088d027cfc9f8c67f017c6b6c66
asafb26 <asafb26@gmail.com>
2015-04-12 01:03:27 +0300

    Employee implementation

---
+++ BL/Employee_BL.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Backend;
+using DAL;
+
+namespace BL
+{
+    public class Employee_BL : IBL
+    {
+        //Fields:
+        IDAL itsDAL;
+
+        //Constructors:
+        public Employee_BL(IDAL dal)
+        {
+            itsDAL = dal;
+        }
+
+        //Methods:
+        public void addEmployee(Backend.Employee e)
+        {
+            //First find conflicts by ID
+            List<Employee> AllEmps = itsDAL.GetAllEmployees();
+            foreach (Employee emp in AllEmps)
+            {
+                if (emp.Id == e.Id)
+                {
+                    throw new System.Data.DuplicateNameException("The ID is already exist in the DB");
+                }
+            }
+            itsDAL.AddEmployee(e);
+        }
+
+    }
+}
---
+++ BL_Backend/Employee.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Backend
+{
+    public enum Gender {Male, Female };
+    public class Employee
+    {
+        //Fields:
+        private int id;
+        private string lastName;
+        private string firstName;
+        private int depID;
+        private int salary;
+        private int supervisiorID;
+        private Gender gender;
+
+        //Constructors:
+        public Employee(string _firstName, string _lastName, int _id, Gender _gender, int _depID, int _salary, int _supervisiorID)
+        {
+            firstName = _firstName;
+            lastName = _lastName;
+            id = _id;
+            gender = _gender;
+            depID = _depID;
+            salary = _salary;
+            supervisiorID = _supervisiorID;
+        }
+        //getters and setters:
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public Gender Gender
+        {
+            get { return gender; }
+            set { gender = value; }
+        }
+
+        public int SupervisiorID
+        {
+            get { return supervisiorID; }
+            set { supervisiorID = value; }
+        }
+
+        public int Salary
+        {
+            get { return salary; }
+            set { salary = value; }
+        }
+
+        public int DepID
+        {
+            get { return depID; }
+            set { depID = value; }
+        }
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+    }
+}
