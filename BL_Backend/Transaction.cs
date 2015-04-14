ede81b85e90caf27478d77a60a9a7d6ab0cd207e
amdurto <amdurto@post.bgu.ac.il>
2015-04-14 19:23:01 +0300

    added tran

---
+++ BL_Backend/Transaction.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL_Backend
+{
+    public enum Is_a_return { Return, Purchase };
+    public enum PaymentMethod { Cash, Check, Visa };
+    class Transaction
+    {
+        //Fields:
+        private int transactionID;
+        private int dateTime;
+        private Is_a_return is_a_return;
+        private Receipt receipt;
+        private PaymentMethod payment;
+
+        //Constructors:
+        public Transaction(int _transactionID, int _dateTime, Is_a_return _is_a_return, Receipt _receipt, PaymentMethod _payment)
+        {
+            transactionID = _transactionID;
+            dateTime = _dateTime;
+            is_a_return = _is_a_return;
+            receipt = _receipt;
+            payment = _payment;
+        }
+        public int ToString()
+        {
+            return transactionID;
+        }
+        //getters and setters:
+        public int Id
+        {
+            get { return transactionID; }
+            set { transactionID = value; }
+        }
+        public int DateTime
+        {
+            get { return dateTime; }
+            set { dateTime = value; }
+        }
+        public Is_a_return Is_a_Return
+        {
+            get { return is_a_return; }
+            set { is_a_return = value; }
+        }
+        public Receipt receipt
+        {
+            get { return receipt; }
+            set { receipt = value; }
+        }
+        public PaymentMethod Payment
+        {
+            get { return payment; }
+            set { payment = value; }
+        }
+    }
+}
