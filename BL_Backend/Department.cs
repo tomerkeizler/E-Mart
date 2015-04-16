1cb4c8f50053dc181d1e3d7853dda4d343db33cf
amdurto <amdurto@post.bgu.ac.il>
2015-04-16 17:39:22 +0300

    added the user class

--- BL_Backend/Department.cs
+++ BL_Backend/Department.cs
@@ -8,6 +8,30 @@ namespace BL_Backend
 {
     class Department
     {
+        //Fields:
+        private string name;
+        private int id;
 
+        //Constructors:
+        public Department(string _name, int _id)
+        {
+            name = _name;
+            id = _id;
+        }
+        public string ToString()
+        {
+            return name;
+        }
+        //getters and setters:
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
     }
 }
