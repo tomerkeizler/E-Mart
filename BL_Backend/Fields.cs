c194d0daa501853a5e6f735886525ff7e30ca736
Asafb26 <asafb26@gmail.com>
2015-04-16 23:37:09 +0300

    comments

--- BL_Backend/Fields.cs
+++ BL_Backend/Fields.cs
@@ -7,8 +7,8 @@ using System.Threading.Tasks;
 namespace Backend
 {
     public enum Elements { Product, Employee };//Add more
-    public enum StringFields { name, firstName, lastName };
-    public enum IntFields { price, stockCount, location, productID, id, depID, salary, supervisiorID };
+    public enum StringFields { name, firstName, lastName };//Add more
+    public enum IntFields { price, stockCount, location, productID, id, depID, salary, supervisiorID };//Add more
     class Fields
     {
     }
