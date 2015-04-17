02fcc25227c6263c222a691a43e5434c70bda7b3
Asaf Ben Aharon <asafb26@gmail.com>
2015-04-17 16:50:44 +0000

    Fields.cs edited online with Bitbucket

--- BL_Backend/Fields.cs
+++ BL_Backend/Fields.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace Backend
 {
-    public enum Elements { Product, Employee, Department, ClubMember, Transaction, User };//Add more
-    public enum StringFields { name, firstName, lastName };
-    public enum IntFields { price, stockCount, location, productID, id, depID, salary, supervisiorID, transactionID };
+    public enum Elements { Product, Employee, Department, ClubMember, Transaction, User };
+    public enum StringFields { name, firstName, lastName, dateOfBirth, username };
+    public enum IntFields { price, stockCount, location, productID, id, depID, salary, supervisiorID, memberID, transactionID };
     public enum Gender { Male, Female };
     class Fields
     {
