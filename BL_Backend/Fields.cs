2fe66bb05b970a1f8d5a42074c67645e524255f1
Asaf Ben Aharon <asafb26@gmail.com>
2015-04-23 19:58:01 +0000

    Fields.cs edited online with Bitbucket

--- BL_Backend/Fields.cs
+++ BL_Backend/Fields.cs
@@ -6,9 +6,10 @@ using System.Threading.Tasks;
 
 namespace Backend
 {
+    public enum IntFields { price, stockCount, location, productID, id, depID, salary, supervisiorID, memberID, transactionID, tranHistory, receipt};
     public enum Elements { Product, Employee, Department, ClubMember, Transaction, User };
     public enum StringFields { name, firstName, lastName, dateOfBirth, username };
-    public enum IntFields { price, stockCount, location, productID, id, depID, salary, supervisiorID, memberID, transactionID, tranHistory, receipt};
+    public enum TypeFields { gender, inStock, type, is_a_return  };
     public enum Gender { Male, Female };
     class Fields
     {
